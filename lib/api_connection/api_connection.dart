251e9385212b5ec1a74f3712fc734ecd492e9b4c
Forkan76 <forkan76@gmail.com>
2023-08-05 13:28:43 +0600

    Update api_connection.dart

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,7 +1,8 @@
 class API {
   // static const hostConnect = "http://127.0.0.1/fbsellers1/api";
-  static const hostConnect = "http://localhost/fbsellers1/api";
-  // static const hostConnect = "https://npkbh.com/api";
+  // static const hostConnect = "http://localhost/fbsellers1/api";
+  static const hostConnect = "https://npkbh.com/api";
+
   static const login = "$hostConnect/login/login.php";
   static const sellerRead = "$hostConnect/seller/read.php";
   static const sellerCustomerRead = "$hostConnect/seller_customer/read.php";
